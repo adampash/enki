c5a3c143c65d61b59e8ba819b9caac8420d9d258
Adam Pash <adam.pash@gmail.com>
2013-08-28 18:45:41 -0400

    added sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -7,5 +7,15 @@ ActionDispatch::ParamsParser::DEFAULT_PARSERS.delete(Mime::YAML)
 ActiveSupport::XmlMini::PARSING.delete("symbol")
 ActiveSupport::XmlMini::PARSING.delete("yaml")
 
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
+
 # Initialize the rails application
 Enki::Application.initialize!
